bffc49d1f4d4d4e1c95ea7d01d47871d72e4925f
halfak <aaron.halfaker@gmail.com>
2015-03-10 14:55:34 -0500

    Updates for deltas refactoring and bumps version to 0.5.1

--- mwstreaming/utilities/dump2diffs.py
+++ mwstreaming/utilities/dump2diffs.py
@@ -32,8 +32,7 @@ import sys
 from multiprocessing import cpu_count
 
 import docopt
-from deltas.detectors import Detector
-from deltas.tokenizers import Tokenizer
+from deltas import DiffEngine
 from mw import xml_dump
 
 import yamlconf
@@ -43,67 +42,64 @@ from .util import op2doc, revision2doc
 
 def main(argv=None):
     args = docopt.docopt(__doc__, argv=argv)
-    
+
     if len(args['<dump_file>']) == 0:
         dump_files = []
     else:
         dump_files = args['<dump_file>']
-    
+
     config_doc = yamlconf.load(open(args['--config']))
-    detector = Detector.from_config(config_doc, config_doc['detector'])
-    tokenizer = yamlconf.import_module(config_doc['tokenizer'])
-    
+    diff_engine = DiffEngine.from_config(config_doc, config_doc['diff_engine'])
+
     drop_text = bool(args['--drop-text'])
-    
+
     if args['--threads'] == "<cpu_count>":
         threads = cpu_count()
     else:
         threads = int(args['--threads'])
-    
-    verbose = bool(args['--verbose'])
-    
-    run(dump_files, detector, tokenizer, threads, verbose)
 
-def run(dump_files, detector, tokenizer, threads, verbose):
-    
+    verbose = bool(args['--verbose'])
+
+    run(dump_files, diff_engine, threads, drop_text, verbose)
+
+def run(dump_files, diff_engine, threads, drop_text, verbose):
+
     if len(dump_files) == 0:
         revision_docs = dump2diffs(xml_dump.Iterator.from_file(sys.stdin),
-                                   detector, tokenizer, verbose=verbose)
-        
+                                   diff_engine, verbose=verbose)
+
     else:
-        dump_processor = lambda d, p: dump2diffs(d, detector, tokenizer,
+        dump_processor = lambda d, p: dump2diffs(d, diff_engine,
                                                  verbose=verbose)
         revision_docs = xml_dump.map(dump_files, dump_processor,
                                      threads=threads)
-    
-        
+
+
     for revision_doc in revision_docs:
+        if drop_text:
+            del revision_doc['text']
+
         json.dump(revision_doc, sys.stdout)
         sys.stdout.write("\n")
 
-def dump2diffs(dump, detector, tokenizer, verbose):
-    
+def dump2diffs(dump, diff_engine, verbose=False):
+
     for page in dump:
-        
+
         if verbose: sys.stderr.write(page.title + ": ")
-        
-        last_tokens = []
+
+        processor = diff_engine.processor()
         for revision in page:
             revision_doc = revision2doc(revision, page)
             if verbose: sys.stderr.write("."); sys.stderr.flush()
-            
-            # Diff detection uses a lot of CPU.  This will be the hottest part
-            # of the code.
-            tokens = list(tokenizer.tokenize(revision_doc['text'] or ""))
-            operations = detector.diff(last_tokens, tokens)
-            
-            revision_doc['diff'] = [op2doc(op, last_tokens, tokens)
-                                    for op in operations]
-            
+
+            # Diff processing uses a lot of CPU.
+            operations, a, b = processor.process(revision_doc['text'] or "")
+
+            revision_doc['diff'] = [op2doc(op, a, b) for op in operations]
+
             yield revision_doc
-            
-            last_tokens = tokens
-        
+
         if verbose: sys.stderr.write("\n")
 
 if __name__ == "__main__": main()
--- mwstreaming/utilities/json2diffs.py
+++ mwstreaming/utilities/json2diffs.py
@@ -20,8 +20,7 @@ import sys
 from itertools import groupby
 
 import docopt
-from deltas.detectors import Detector
-from deltas.tokenizers import Tokenizer
+from deltas import DiffEngine
 
 import yamlconf
 
@@ -30,50 +29,41 @@ from .util import op2doc, read_docs
 
 def main(argv=None):
     args = docopt.docopt(__doc__, argv=argv)
-    
+
     config_doc = yamlconf.load(open(args['--config']))
-    detector = Detector.from_config(config_doc, config_doc['detector'])
-    tokenizer = Tokenizer.from_config(config_doc, config_doc['tokenizer'])
-    
+    diff_engine = DiffEngine.from_config(config_doc, config_doc["diff_engine"])
+
     drop_text = bool(args['--drop-text'])
     verbose = bool(args['--verbose'])
-    
-    run(read_docs(sys.stdin), detector, tokenizer, drop_text, verbose)
 
-def run(revision_docs, detector, tokenizer, drop_text, verbose):
-    
-    for revision_doc in json2diffs(revision_docs, detector, tokenizer, verbose):
+    run(read_docs(sys.stdin), diff_engine, drop_text, verbose)
+
+def run(revision_docs, diff_engine, drop_text, verbose):
+
+    for revision_doc in json2diffs(revision_docs, diff_engine, verbose):
         if drop_text:
             del revision_doc['text']
-        
+
         json.dump(revision_doc, sys.stdout)
         sys.stdout.write("\n")
 
-def json2diffs(revision_docs, detector, tokenizer, verbose):
-    
+def json2diffs(revision_docs, diff_engine, verbose=False):
+
     page_revision_docs = groupby(revision_docs, key=lambda r:r['page']['title'])
-    
+
     for page_title, revision_docs in page_revision_docs:
-        
         if verbose: sys.stderr.write(page_title + ": ")
-        
-        last_tokens = []
+
+        processor = diff_engine.processor()
         for revision_doc in revision_docs:
             if verbose: sys.stderr.write("."); sys.stderr.flush()
-            
-            # Diff detection uses a lot of CPU.  This will be the hottest part
-            # of the code.
-            tokens = tokenizer.tokenize(revision_doc['text'] or "")
-            operations = detector.diff(last_tokens, tokens)
-            
-            revision_doc['diff'] = [op2doc(op, last_tokens, tokens)
-                                    for op in operations]
-            
+
+            # Diff processing uses a lot of CPU.
+            operations, a, b = processor.process(revision_doc['text'] or "")
+            revision_doc['diff'] = [op2doc(op, a, b) for op in operations]
+
             yield revision_doc
-            
-            last_tokens = tokens
-        
-        
+
         if verbose: sys.stderr.write("\n")
 
 if __name__ == "__main__": main()
